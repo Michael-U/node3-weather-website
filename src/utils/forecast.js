7ff78e612c578b43909eea90a6e7aad32b0e7b51
michael Uche Okafor <michaeluche.okafor@gmail.com>
2020-01-09 10:10:52 +0100

    Add High and Low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,10 @@ else if(body.error){
   callback('Unable to find location',undefined)
 }
 else {
-  callback(undefined,`${body.daily.data[0].summary} it is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} % chance of rain.`)
+
+          
+          callback(undefined,`${body.daily.data[0].summary} it is currently ${body.currently.temperature} degrees out. The high today 
+          ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is a ${body.currently.precipProbability} % chance of rain.`);
 }
 
 })
